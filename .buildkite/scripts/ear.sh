aff4d6a26c20bf7a849a4f5ce5ef2e58fff1f776
Brian Seeders <brian.seeders@elastic.co>
2023-07-24 15:15:32 -0400

    Fix variable escapes

--- .buildkite/scripts/ear.sh
+++ .buildkite/scripts/ear.sh
@@ -7,19 +7,19 @@ set -euo pipefail
 set -e
 dd if=/dev/zero of=dm-crypt.img bs=1 count=0 seek=80GB
 dd if=/dev/urandom of=key.secret bs=2k count=1
-LOOP=$$(losetup -f)
-sudo losetup $$LOOP dm-crypt.img
-sudo cryptsetup luksFormat -q --key-file key.secret "$$LOOP"
-sudo cryptsetup open --key-file key.secret "$$LOOP" secret --verbose
+LOOP=$(losetup -f)
+sudo losetup $LOOP dm-crypt.img
+sudo cryptsetup luksFormat -q --key-file key.secret "$LOOP"
+sudo cryptsetup open --key-file key.secret "$LOOP" secret --verbose
 sudo mkfs.ext2 /dev/mapper/secret
 sudo mkdir /mnt/secret
 # Change /mnt/secret with care (at least a test uses this path to detect when encryption at rest is used)
 sudo mount /dev/mapper/secret /mnt/secret
 sudo chown -R buildkite-agent /mnt/secret
-cp -r "$$WORKSPACE" /mnt/secret
-cd /mnt/secret/$$(basename "$$WORKSPACE")
+cp -r "$WORKSPACE" /mnt/secret
+cd /mnt/secret/$(basename "$WORKSPACE")
 touch .output.log
-rm -Rf "$$WORKSPACE"
-ln -s "$$PWD" "$$WORKSPACE"
+rm -Rf "$WORKSPACE"
+ln -s "$PWD" "$WORKSPACE"
 
 .ci/scripts/run-gradle.sh -Dbwc.checkout.align=true check
--- .buildkite/scripts/eql-correctness.sh
+++ .buildkite/scripts/eql-correctness.sh
@@ -3,10 +3,10 @@
 set -euo pipefail
 
 set +x
-#VAULT_TOKEN=$$(vault write -field=token auth/approle/login role_id=$$VAULT_ROLE_ID secret_id=$$VAULT_SECRET_ID)
+#VAULT_TOKEN=$(vault write -field=token auth/approle/login role_id=$VAULT_ROLE_ID secret_id=$VAULT_SECRET_ID)
 export VAULT_TOKEN
-export eql_test_credentials_file="$$(pwd)/x-pack/plugin/eql/qa/correctness/credentials.gcs.json"
-vault read -field=credentials.gcs.json secret/ci/elastic-elasticsearch/migrated/eql_test_credentials > $${eql_test_credentials_file}
+export eql_test_credentials_file="$(pwd)/x-pack/plugin/eql/qa/correctness/credentials.gcs.json"
+vault read -field=credentials.gcs.json secret/ci/elastic-elasticsearch/migrated/eql_test_credentials > ${eql_test_credentials_file}
 #unset VAULT_TOKEN
 set -x
 
