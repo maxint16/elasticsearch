f591774b211c540279505a4800606bda406f7bbb
Brian Seeders <brian.seeders@elastic.co>
2023-07-27 14:48:25 -0400

    WIP lucene_snapshot automation

---
+++ .buildkite/scripts/lucene-snapshot/update-branch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+if [[ "$BUILDKITE_BRANCH" != "lucene_snapshot" ]]; then
+  echo "Error: This script should only be run on the lucene_snapshot branch"
+  exit 1
+fi
+
+git config --global user.name elasticmachine
+git config --global user.email '15837671+elasticmachine@users.noreply.github.com'
+
+git fetch origin main
+git merge origin/main
+git push origin lucene_snapshot
